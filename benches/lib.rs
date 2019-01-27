6442e350a28220137396d4b4add9779b64e76bb8
Vladimir Ignatev <ya.na.pochte@gmail.com>
2019-01-27 22:00:44 +0400

    also added a bench for shorter input

--- benches/lib.rs
+++ benches/lib.rs
@@ -10,6 +10,13 @@ mod tests {
     use test::Bencher;
 
     #[bench]
+    fn bench_encode_512bits(b: &mut Bencher) {
+        b.iter(|| {
+            hex::encode("5d07719b61b0abb6f1c3b17b1d69c838278f87f9b5e75077026e5fedf96c2eb25d07719b61b0abb6f1c3b17b1d69c838278f87f9b5e75077026e5fedf96c2eb2")
+        });
+    }
+
+    #[bench]
     fn bench_encode_256bits(b: &mut Bencher) {
         b.iter(|| {
             hex::encode("5d07719b61b0abb6f1c3b17b1d69c838278f87f9b5e75077026e5fedf96c2eb2")
@@ -34,6 +41,13 @@ mod tests {
     fn bench_encode_32bits(b: &mut Bencher) {
         b.iter(|| {
             hex::encode("5d07719b")
+        });
+    }
+
+    #[bench]
+    fn bench_encode_16bits(b: &mut Bencher) {
+        b.iter(|| {
+            hex::encode("5d07")
         });
     }
 }
