caf8a6048e3e49f06d6f79ea6151c07241d9da6c
Vladimir Ignatev <ya.na.pochte@gmail.com>
2019-01-27 21:43:20 +0400

    added simple benches to check performance across different input sizes for hex::encode

---
+++ benches/lib.rs
@@ -0,0 +1,39 @@
+#![feature(test)]
+
+extern crate test;
+extern crate hex;
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_encode_256bits(b: &mut Bencher) {
+        b.iter(|| {
+            hex::encode("5d07719b61b0abb6f1c3b17b1d69c838278f87f9b5e75077026e5fedf96c2eb2")
+        });
+    }
+
+    #[bench]
+    fn bench_encode_128bits(b: &mut Bencher) {
+        b.iter(|| {
+            hex::encode("5d07719b61b0abb6f1c3b17b1d69c838")
+        });
+    }
+
+    #[bench]
+    fn bench_encode_64bits(b: &mut Bencher) {
+        b.iter(|| {
+            hex::encode("5d07719b61b0abb6")
+        });
+    }
+
+    #[bench]
+    fn bench_encode_32bits(b: &mut Bencher) {
+        b.iter(|| {
+            hex::encode("5d07719b")
+        });
+    }
+}
